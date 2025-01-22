506aec1def78f1c19c0e7c4ed6bba0cbf5410d55
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-01-21 23:47:37 -0300

    Update return.py

--- return.py
+++ return.py
@@ -1,9 +1,15 @@
 
-def cube(num):
-    return num*num*num
+# def cube(num):
+  #  return num*num*num
 
+# result = cube(4)
+# print(result)
 
-result = cube(4)
-print(result)
+# While loop
 
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
 
+print("Done with loop")
